7e2d9b6f5d11775d8f95d230c68a8fa75fb7897d
maxwellagyapong <agyapongmaxwell26@gmail.com>
2023-08-01 13:46:38 +0100

    Configured endpoints for various views

--- catalog/product/urls.py
+++ catalog/product/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from .views import ProductList, ProductCreate, ProductDetail
 
 urlpatterns = [
-    
+    path('', ProductList.as_view(), name='products'),
+    path('create/', ProductCreate.as_view(), name='create-product'),
+    path('<int:pk>/', ProductDetail.as_view(), name='product-detail')
 ]
