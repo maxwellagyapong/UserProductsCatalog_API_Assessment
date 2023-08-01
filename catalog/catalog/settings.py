f78f13a3ad4905fa67e2e47c46de7392cc6219f8
maxwellagyapong <agyapongmaxwell26@gmail.com>
2023-08-01 12:50:24 +0100

    Started product app

--- catalog/catalog/settings.py
+++ catalog/catalog/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # local apps
+    'product',
+    
+    # install apps
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
