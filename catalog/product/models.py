9ce5a8f0442083ff3c6b03cb3b0767e8cf3291b1
maxwellagyapong <agyapongmaxwell26@gmail.com>
2023-08-01 14:05:14 +0100

    Added temporary User, and api-auth routes so I can work on permission

--- catalog/product/models.py
+++ catalog/product/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.core.validators import MinValueValidator
+from django.contrib.auth.models import User
 
 class Product(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=15, decimal_places=2, 
                                 validators=[MinValueValidator(0)])
