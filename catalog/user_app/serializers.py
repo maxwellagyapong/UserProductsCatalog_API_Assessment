f6e648a038d32f9d2be16c5c6cf09ab4115dde22
maxwellagyapong <agyapongmaxwell26@gmail.com>
2023-08-03 20:31:55 +0100

    Added register functionality

---
+++ catalog/user_app/serializers.py
@@ -0,0 +1,34 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField(style={'input_type': 'password'}, write_only=True)
+    
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'password2']
+        extra_kwargs = {
+            'password': {'write_only': True} 
+        }
+        
+    def save(self, **kwargs):
+        
+        password = self.validated_data.get('password')
+        password2 = self.validated_data.get('password2')
+        
+        if password != password2:
+            raise serializers.ValidationError({"Error": "P1 and P2 must be the same!"})
+        
+        if User.objects.filter(email=self.validated_data.get('email')).exists:
+            raise serializers.ValidationError({"Error": "Email already exists!"})
+        
+        account = User(email=self.validated_data.get("email"), 
+                       username=self.validated_data.get('username'),)
+        account.set_password(password)
+        
+        account.save()
+        
+        return account
+        
+        
+        
--- catalog/user_app/views.py
+++ catalog/user_app/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.views import Response
+from rest_framework.decorators import api_view
+from .serializers import RegisterSerializer
 
-# Create your views here.
+@api_view(["POST"])
+def register_view(request):
+    
+    if request.method == "POST":
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
