b08ec49aa3b5114f2c12820fe4a320eb6cdfb6d0
maxwellagyapong <agyapongmaxwell26@gmail.com>
2023-08-03 20:46:41 +0100

    Added logout functionality

--- catalog/user_app/urls.py
+++ catalog/user_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
-from .views import register_view
+from .views import register_view, logout_view
 
 urlpatterns = [
     path('login/', obtain_auth_token, name='login'),
     path('register/', register_view, name='register'),
+    path('logout/', logout_view, name='logout'),
 ]
--- catalog/user_app/views.py
+++ catalog/user_app/views.py
@@ -1,6 +1,9 @@
 from rest_framework.views import Response
 from rest_framework.decorators import api_view
 from .serializers import RegisterSerializer
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
 
 @api_view(["POST"])
 def register_view(request):
@@ -12,3 +15,11 @@ def register_view(request):
             return Response(serializer.data)
         else:
             return Response(serializer.errors)
+
+@api_view(["POST",])
+@permission_classes([IsAuthenticated]) 
+def logout_view(request):
+    
+    if request.method == "POST":
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_200_OK)
