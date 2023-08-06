47e26c874d5ad9026770e0d51c1235f800eb4b2a
maxwellagyapong <agyapongmaxwell26@gmail.com>
2023-08-06 07:35:16 +0100

    Added redis related configuration for celery

--- catalog/catalog/settings.py
+++ catalog/catalog/settings.py
@@ -149,3 +149,10 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
 }
+
+# REDIS related settings 
+REDIS_HOST = 'localhost' 
+REDIS_PORT = '6379' 
+BROKER_URL = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0' 
+BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 3600} 
+CELERY_RESULT_BACKEND = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
