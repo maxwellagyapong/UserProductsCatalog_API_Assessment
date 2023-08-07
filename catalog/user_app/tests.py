af235e86fdc54db0b0e77e89ee567935e5409365
maxwellagyapong <agyapongmaxwell26@gmail.com>
2023-08-07 14:40:56 +0100

    Added automated tests for login and logout

--- catalog/user_app/tests.py
+++ catalog/user_app/tests.py
@@ -18,3 +18,22 @@ class RegisterTestCase(APITestCase):
         response = self.client.post(reverse('register'), data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         
+
+class LoginLogoutTestCase(APITestCase):
+    
+    def setUp(self):
+        self.user = User.objects.create_user(email="test@gmail.com", first_name="test",
+                                        last_name="case", password="Password@123",)
+    
+    def test_login(self):
+        
+        data = {
+            "email": "test@gmail.com",
+            "password": "Password@123"
+        }
+        
+        response = self.client.post(reverse("login"), data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+    def test_logout(self):
+        pass
