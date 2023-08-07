92c46ce9acf74e25bb5cfdb6b709b2b8379e7129
maxwellagyapong <agyapongmaxwell26@gmail.com>
2023-08-07 14:33:23 +0100

    Added automated tests for user registration

--- catalog/user_app/tests.py
+++ catalog/user_app/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from .models import User
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
 
-# Create your tests here.
+class RegisterTestCase(APITestCase):
+    
+    def test_register(self):
+        
+        data = {
+            "email": "testcase@gmail.com",
+            "first_name": "test",
+            "last_name": "case",
+            "password": "Password@123",
+            "password2": "Password@123",
+        }
+        
+        response = self.client.post(reverse('register'), data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        
